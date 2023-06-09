e4339e75448620de6f644ae9a88ab2cc25e65af8
kukuhsetiadi221 <kukuhsetiadi@students.amikom.ac.id>
2023-06-09 22:27:13 +0700

    first commit

---
+++ Printer.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tugas_nama_printer
+{
+    public class Printer
+    {
+        public string NamaPrinter { get; set; }
+        public virtual void Merek()
+        {
+            Console.WriteLine("Merek printer");
+        }
+    }
+
+    public class Epson : Printer
+    {
+        public override void Merek()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Epson display dimension : 10*11");
+            Console.WriteLine("Epson printer printing....");
+        }
+    }
+    public class Canon : Printer
+    {
+        public override void Merek()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Canon display dimension : 9.5*12");
+            Console.WriteLine("Canon printer printing....");
+        }
+    }
+    public class LasetJet : Printer
+    {
+        public override void Merek()
+        {
+            Console.WriteLine();
+            Console.WriteLine("LasetJet display dimension : 12*12");
+            Console.WriteLine("LasetJet printer printing....");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tugas_nama_printer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Printer Printer;
+            Console.WriteLine("Pilih printer:");
+            Console.WriteLine("1. Epson");
+            Console.WriteLine("2. Canon");
+            Console.WriteLine("3. LasetJet");
+            Console.WriteLine();
+            Console.Write("Nomor printer [1..3]: ");
+            
+            Printer Epson = new Epson();
+            Printer Canon = new Canon();
+            Printer LasetJet = new LasetJet();
+
+            int nomorprinter = Convert.ToInt32(Console.ReadLine());
+
+            if (nomorprinter == 1)
+                Epson.Merek();
+            else if (nomorprinter == 2)
+                Canon.Merek();
+            else if (nomorprinter == 3)
+                LasetJet.Merek();
+            else
+                Console.WriteLine("Nomor printer tidak valid");
+
+            Console.ReadKey();
+        }
+    }
+}
